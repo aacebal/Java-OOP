e83c2565b141b26b06c9e859b08c9da1222311f6
Adel Acebal <adelacebal@yahoo.com>
2017-09-12 20:23:12 -0400

    completing challenge

--- ArrayListChallenge/src/com/adelacebal/Contact.java
+++ ArrayListChallenge/src/com/adelacebal/Contact.java
@@ -2,22 +2,23 @@ package com.adelacebal;
 
 public class Contact {
 
-    public String name;
-    public String phoneNumber;
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public static Contact createContact(String name, String phoneNumber) {
+        return new Contact(name, phoneNumber);
     }
 }
--- ArrayListChallenge/src/com/adelacebal/MobilePhone.java
+++ ArrayListChallenge/src/com/adelacebal/MobilePhone.java
@@ -4,27 +4,54 @@ import java.util.ArrayList;
 
 public class MobilePhone {
 
-    ArrayList<Contact> contacts = new ArrayList<>();
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
 
-    public void addContact(Contact contact) {
-        contacts.add(contact);
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
     }
 
-    public void modifyContact(String oldContactName, Contact newContact) {
-        int oldContactIndex = findContact(oldContactName);
-        contacts.set(oldContactIndex, newContact);
+    public boolean addContact(Contact contact) {
+        if (findContact(contact.getName()) >= 0) {
+            System.out.println("Contact is already on file");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact) {
+        int oldContactIndex = findContact(oldContact);
+        if (oldContactIndex < 0) {
+            System.out.println(oldContact.getName() + " was not found");
+            return false;
+        }
+        this.myContacts.set(oldContactIndex, newContact);
+        System.out.println(oldContact.getName() + " was replaced with " + newContact.getName());
+        return true;
     }
 
     public void removeContact(String nameToRemove) {
         int indexToRemove = findContact(nameToRemove);
-        contacts.remove(indexToRemove);
+        myContacts.remove(indexToRemove);
     }
 
-    private int findContact(String nameToFind) {
-        int indexOfInfo = contacts.indexOf(nameToFind);
+    private int findContact(Contact contact) {
+        int indexOfInfo = myContacts.indexOf(contact);
 
         if (indexOfInfo >= 0) {
             return indexOfInfo;
+        }
+        return -1;
+    }
+
+    private int findContact(String contactName) {
+        for (int i=0; i<this.myContacts.size(); i++) {
+            Contact contact = this.myContacts.get(i);
+            if (contact.getName().equals(contactName)) {
+                return i;
+            }
         }
         return -1;
     }
